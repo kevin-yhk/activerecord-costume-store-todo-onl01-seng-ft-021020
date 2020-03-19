6c690f2188bc35ea8c1b41455908ef67ee4f7a7b
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-19 02:42:33 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,4 +8,5 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 class CreateCostumes < ActiveRecord::Migration[5.2]
-  
+  def change
+    create_table 

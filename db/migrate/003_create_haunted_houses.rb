1e8cb54359df99b77877c09955007c7b9aae2c54
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-19 02:56:59 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,9 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
-    create_table 
+    create_table haunted_houses do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme 
+      t.
 end 

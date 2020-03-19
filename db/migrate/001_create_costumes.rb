46fb0ea73d7727f94305b836259b50df68f36fd3
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-19 02:44:27 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,4 +11,6 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
       t.string :name 
-      t.st 
+      t.integer :price
+      t.string :size 
+      t.string :image

32a9ac7f4d377abe6d575d5e2d391a1d77df13e9
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-19 02:57:56 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,5 +6,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location
       t.string :theme 
       t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.
 end 

dba2a852286493dbba018ee560310f3b63bf6be8
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-19 02:50:27 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,5 +5,6 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name 
       t.string :location
       t.integer :costume_inventory
-      t.integer 
+      t.integer :num_of_employees
+      t.
 end 
